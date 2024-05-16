537c9b9e3fa13b21e15f90935d33a173f898602c
Avanthika0903 <150774721+Avanthika0903@users.noreply.github.com>
2024-05-16 10:31:50 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,10 +1,2 @@
-provider "aws" {
-  region = "eu-north-1"
-  access_key="AKIAZI2LCIQYA6PYZBMT"
-  secret_key="ZajfA1B6XyZjBj1Wr1F0D5YSQfQvUXxmE9MGm/8s"
-}
+provider "aws" { }
 
-resource "aws_instance" "example" {
-  ami           = "ami-03c3351e3ce9d04eb"
-  instance_type = "t3.micro"
-}
